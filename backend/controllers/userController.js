66a92fdd410a35e626155568ca96a85ecd653abf
mukesh307 <156671892+mukesh307@users.noreply.github.com>
2025-05-06 12:12:27 +0530

    Reinstalled bcrypt to fix Render deployment issue

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const nodemailer = require('nodemailer');
 const sendEmail = require("../utils/sendEmail");
 const {sendSMS} = require("../utils/sendSMS");
